3391c0f1be9c4b5efc0140b8bf52472557d2ec39
Shrishtibansal <shrishtibansal2305@gmail.com>
2026-08-24 21:04:30 +0530

    Update hospital finder project

---
+++ test_api.py
@@ -0,0 +1,5 @@
+﻿import requests
+
+response = requests.get("https://api.agify.io", params={"name": "shrishti"})
+data = response.json()
+print(data)
---
+++ test_osm.py
@@ -0,0 +1,15 @@
+﻿import requests
+
+url = "https://nominatim.openstreetmap.org/search"
+params = {
+    "q": "hospital in Jaipur",
+    "format": "json",
+    "limit": 5
+}
+headers = {
+    "User-Agent": "hospital-finder-app"
+}
+
+response = requests.get(url, params=params, headers=headers)
+data = response.json()
+print(data)
